b886b4ac2688fee209d730cedbcdb41f90498553
Willian Antunes <willian.lima.antunes@gmail.com>
2016-08-13 18:23:20 -0300

    XmlMapper does not cast with ObjectMapper if XML is used as producer

--- src/main/java/br/com/willianantunes/test/cxf/resolver/ObjectMapperContextResolver.java
+++ src/main/java/br/com/willianantunes/test/cxf/resolver/ObjectMapperContextResolver.java
@@ -7,9 +7,10 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
- * 
  * @author Willian Antunes
  * @see <a href="http://stackoverflow.com/questions/29571587/deserializing-localdatetime-with-jackson-jsr310-module">Deserializing LocalDateTime with Jackson JSR310 module</a>
+ * @see <a href="http://stackoverflow.com/questions/35367366/can-you-set-jackons-include-non-null-globally-in-jax-rs">Can you set Jackon's Include.NON_NULL globally in JAX-RS?</a>
+ * 
  */
 public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
 	
